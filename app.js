840f7ecf5dceb08d06c7bf437976622a8a78f631
Rakib0061 <rakibulislam0061@gmail.com>
2023-02-15 11:01:51 +0600

    media query by js in md and sm deveice

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ let item = document.getElementById("butterfly");
 function anime(){
     t ++;
 
-    posX = 650 + (Math.sin(t / 100 *3* Math.PI * 1/10) * 500);
+    posX = 550 + (Math.sin(t / 150 *3* Math.PI * 1/10) * 550);
     posY = Math.sin(t / 5 * Math.PI / 1/3.9) * 30;
 
     item.style.left = posX +"px";
@@ -76,13 +76,139 @@ anime();
 
 
 
-//                                      Butterfly media effect
+//                                      Butterfly media query
+
+
+
+let mqls = [
+    window.matchMedia('screen and (min-width: 576px) and (max-width: 767px'), /* md */
+    window.matchMedia('screen and (min-width: 768px) and (max-width: 991px'), /* lg */
+    window.matchMedia('screen and (min-width: 992px) and (max-width: 1199px)') /* xl */
+];
+
+for(let i=0; i < mqls.length; i++){
+    mqls[i].addEventListener("change",()=>{
+
+        if( mqls[2].matches ){
+            /******************** xl ********************/
+
+            let posX = 0;
+            let posY = 0;
+            let t = 0.1;
+            let item = document.getElementById("butterfly");
+
+
+            function anime(){
+            t ++;
+
+                posX = 650 + (Math.sin(t / 100 *3* Math.PI * 1/10) * 250);
+                posY = Math.sin(t / 5 * Math.PI / 1/3.9) * 30;
+
+                item.style.left = posX +"px";
+                item.style.bottom = posY + "px";
+
+                requestAnimationFrame(anime);
+            }
+
+            anime();
+    
+            console.log('xl');   
+
+            /******************** xl ********************/
+        }
+    
+        else if( mqls[1].matches ){
+
+            /******************** lg ********************/
+
+            let posX = 0;
+            let posY = 0;
+            let t = 0.1;
+            let item = document.getElementById("butterfly");
+
+
+            function anime(){
+            t ++;
+
+                posX = 650 + (Math.sin(t / 100 *3* Math.PI * 1/10) * 70);
+                posY = Math.sin(t / 5 * Math.PI / 1/3.9) * 30;
+
+                item.style.left = posX +"px";
+                item.style.bottom = posY + "px";
+
+                requestAnimationFrame(anime);
+            }
+
+            anime();
+
+            console.log('lg'); 
+
+            /******************** lg ********************/
+        }
+        else if( mqls[0].matches ){
+
+            /******************** md ********************/
+
+            let posX = 0;
+            let posY = 0;
+            let t = 0.1;
+            let item = document.getElementById("butterfly");
+
+
+            function anime(){
+            t ++;
+
+                posX = 260 + (Math.sin(t / 100 *3* Math.PI * 1/10) * 190);
+                posY = Math.sin(t / 5 * Math.PI / 1/3.9) * 30;
+
+                item.style.left = posX +"px";
+                item.style.bottom = posY + "px";
+
+                requestAnimationFrame(anime);
+            }
+
+            anime();
+
+            console.log('md');  
+
+            /******************** md ********************/
+        }
+        else if( (!mqls[2].matches && !mqls[1].matches) && !mqls[0].matches){
+
+            /******************** sm ********************/
+
+            let posX = 0;
+            let posY = 0;
+            let t = 0.1;
+            let item = document.getElementById("butterfly");
+
+
+            function anime(){
+            t ++;
+
+                posX = 170 + (Math.sin(t / 100 *3* Math.PI * 1/10) * 150);
+                posY = Math.sin(t / 5 * Math.PI / 1/3.9) * 30;
+
+                item.style.left = posX +"px";
+                item.style.bottom = posY + "px";
+
+                requestAnimationFrame(anime);
+            }
+
+            anime();
+    
+            console.log('sm');  
+
+            /******************** sm ********************/
+        }
+    });
+}
 
 
 
 
 
-//                                      Butterfly media effect
+//                                      Butterfly media query
 
 
 
